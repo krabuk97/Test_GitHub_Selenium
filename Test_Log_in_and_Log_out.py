7fb616b39cbb9bfa91e6ff546a54b8a6736450ab
Ruslan Cheberdyn <125387726+krabuk97@users.noreply.github.com>
2023-05-01 21:38:57 +0200

    Update Test_Log_in_and_Log_out.py

--- Test_Log_in_and_Log_out.py
+++ Test_Log_in_and_Log_out.py
@@ -19,9 +19,20 @@ def test_Log_in():
 
     button_Sign_in = driver.find_element(By.NAME,"commit")
     button_Sign_in.click()
-    print("Test Success!!!")
+    print("Test 1 Success!!!")
 
 
-    time.sleep(5)
+def new_repository():
+    # Find new repository button
+    new = driver.find_element(By.CLASS_NAME, "dropdown-caret")
+    new.click()
+
+    #Find and press new repository button
+    newrep = driver.find_element(By.XPATH, '//a[@href="/new" and contains(@data-ga-click, "create new repository")]')
+    newrep.click()
+    time.sleep(10)
+
+    print("Test 2 Success!!!")
 
 test_Log_in()
+new_repository()
